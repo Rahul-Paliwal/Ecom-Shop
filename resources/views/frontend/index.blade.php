651b3b3ebde0ed94426dc079e97831681952aedb
rahul <rahulpaliwal177@gmail.com>
2021-07-24 13:12:36 +0530

    Product Tag Show

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -206,14 +206,7 @@ Ecom Shop
         <!-- /.sidebar-widget --> 
         <!-- ============================================== SPECIAL OFFER : END ============================================== --> 
         <!-- ============================================== PRODUCT TAGS ============================================== -->
-        <div class="sidebar-widget product-tag wow fadeInUp">
-          <h3 class="section-title">Product tags</h3>
-          <div class="sidebar-widget-body outer-top-xs">
-            <div class="tag-list"> <a class="item" title="Phone" href="category.html">Phone</a> <a class="item active" title="Vest" href="category.html">Vest</a> <a class="item" title="Smartphone" href="category.html">Smartphone</a> <a class="item" title="Furniture" href="category.html">Furniture</a> <a class="item" title="T-shirt" href="category.html">T-shirt</a> <a class="item" title="Sweatpants" href="category.html">Sweatpants</a> <a class="item" title="Sneaker" href="category.html">Sneaker</a> <a class="item" title="Toys" href="category.html">Toys</a> <a class="item" title="Rose" href="category.html">Rose</a> </div>
-            <!-- /.tag-list --> 
-          </div>
-          <!-- /.sidebar-widget-body --> 
-        </div>
+          @include('frontend.common.product_tags')
         <!-- /.sidebar-widget --> 
         <!-- ============================================== PRODUCT TAGS : END ============================================== --> 
         <!-- ============================================== SPECIAL DEALS ============================================== -->
@@ -829,7 +822,7 @@ Ecom Shop
                     <div class="products">
                       <div class="product">
                         <div class="product-image">
-                          <div class="image"> <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug_en ) }}"><img  src="{{ asset($product->product_thambnail) }}" alt=""></a> </div>
+                          <div class="image"> <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug_en ) }}"><img  src="{{ asset($product->product_thumbnil) }}" alt=""></a> </div>
                           <!-- /.image -->
 
         @php
